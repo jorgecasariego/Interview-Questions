8023e799d5424947ca64bad517b6bd0b75de0199
Jorge Casariego <jorgekasa@gmail.com>
2017-10-16 12:48:57 -0300

    FizzBuzz implementation

---
+++ FizzBuzz/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,37 @@
+
+//Prints numbers 1 - 100, 25 values per line.
+//Numbers that are multiples of 3 are replaced with Fizz
+//Numbers that are mulitples of 5 are replaced with Buzz
+//Numbers that are multiples of both 3 and 5 are replaced with FizzBuzz
+public class FizzBuzz {
+
+	public static void fizzBuzz(int number){
+		StringBuilder textToPrint = new StringBuilder();
+		
+		// Time complexity is O(n).
+		for(int i=1; i<=number; i++){
+			if(i % 15 == 0){
+				textToPrint.append("FizzBuzz");
+			} else if(i % 3 == 0){
+				textToPrint.append("Fizz");
+			} else if(i % 5 == 0 ){
+				textToPrint.append("Buzz");
+			} else {
+				textToPrint.append(String.valueOf(i));
+			}
+			
+			textToPrint.append(" ");
+			
+			if(textToPrint.length() % 25 == 0){
+				textToPrint.append("\n");
+			}
+		}
+		
+		System.out.println(textToPrint);
+		
+	}
+	
+	public static void main(String[] args){
+		fizzBuzz(15);
+	}
+}
---
+++ FizzBuzz/README.md
@@ -0,0 +1,3 @@
+# FizzBuzz
+
+In this question, you will work with two numbers 3 and 5. The user gives a number as input and then we'll have to get the list of all the numbers starting from 1 till the given number. Now, all we need to do is to find the numbers that are multiples of 3 and 5 and print special words called Fizz and Buzz instead of those multiples respectively. If a number is multiple of both 3 as well as 5 (say 15) FizzBuzz should be printed and if a number is neither a multiple of 3 nor 5 then the number itself is printed.
