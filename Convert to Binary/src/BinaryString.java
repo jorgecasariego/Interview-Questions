a4a89a31f67a1afb7baef5323959fd670e3349f9
Jorge Casariego <jorgekasa@gmail.com>
2017-11-16 22:34:28 -0300

    Example to convert an Int to Binary using recursion

--- Convert to Binary/src/BinaryString.java
+++ Convert to Binary/src/BinaryString.java
@@ -24,6 +24,34 @@ public class BinaryString {
 		}
 	}
 	
+	// Doing recursively
+	public static void integerToBinary(int n) {
+		if(n < 2){
+			System.out.print(n);
+		}else  {
+			/*
+			 * 43 to binary is:                   101011
+			 * integerToBinary of 21 (43 / 2) ==> 10101
+			 * 		integerToBinary of 10
+			 * 			integerToBinary of 5
+			 * 				integerToBinary of 2
+			 * 					integerToBinary of 1
+			 * 					integerToBinary of 0
+			 * 				integerToBinary of 1
+			 * 			integerToBinary of 0
+			 * 		integerToBinary of 1
+			 * +
+			 * integerToBinary of  1 (43 % 2) ==>      1
+			 */
+			int lastDigit = n % 2;
+			int restOfDigit = n / 2;
+			integerToBinary(restOfDigit);
+			integerToBinary(lastDigit);
+			
+		}
+			
+	}
+	
 	public static void main(String[] args){
 		BinaryString example = new BinaryString("Hello");
 		System.out.println("");
@@ -31,5 +59,8 @@ public class BinaryString {
 		BinaryString number = new BinaryString(4);
 		
 		BinaryString binaryStringOR = new BinaryString("HelloWord", 8675309);
+		
+		System.out.println("43 to binary is: ");
+		integerToBinary(43);
 	}
 }
