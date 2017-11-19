ae217507f9b004c151c9f265f2ab44cbe0665dc6
Jorge Casariego <jorgekasa@gmail.com>
2017-11-19 19:31:23 -0300

    Counting inversions implemented with MergeSort and QuickSort

--- Merge Sort - Counting Inversions/src/Solution.java
+++ Merge Sort - Counting Inversions/src/Solution.java
@@ -1,11 +1,78 @@
-
+/**
+ * Inversion Count for an array indicates – how far (or close) the array is from being sorted. 
+ * If array is already sorted then inversion count is 0. 
+ * If array is sorted in reverse order that inversion count is the maximum. 
+ * 
+ * 
+ * @author jorgecasariego
+ *
+ */
 public class Solution {
+	
+	static int inversions;
+	
+	public static void countInversionsQuick(int[] array) {
+		quicksort(array, 0, array.length-1);
+	}
+	
+	public static void quicksort(int[] array, int l, int r){
+		if(l >= r){
+			return;
+		}
+		
+		int pivot = (l + r) / 2;
+		int index = partition(array, l, r, pivot);
+		
+		quicksort(array, l, index - 1);
+		quicksort(array, index, r);
+		
+	}
+	
+	// TEST
+	// 		9, 2, 6, 4, 3, 5, 1		-->  1, 2, 6, 4, 3, 5, 9
+	// 		^		 ^	      ^			    ^     ^	    ^
+	// 	   left		pivot	right		  left	pivot  right
+	public static int partition(int[] array, int l, int r, int pivot){
+		while(l <= r){
+			if(array[l] < pivot){
+				l++;
+			}
+			
+			if(array[r] >= pivot){
+				r--;
+			}
+			
+			if(l <= r){
+				swap(array, l, r);
+				inversions++;
+				l++;
+				r--;
+			}
+		}
+		
+		return l;
+	}
+	
+	public static void swap(int[] array, int i, int j){
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	//************************************************************************
 	
 	static long countInversions(int[] arr) {
         return mergesort(arr, 0, arr.length-1);
         
     }
    
+	// {2, 1, 3, 1, 2};
+	// m = (0 + 4) / 2 = 2
+	// ms(0, 2)
+	//	m = (0+2)/2 = 1
+	//	ms(0, 1)
+	//		m = (0+1)/2 = 0
+	// 		
 	static long mergesort(int[] array, int l, int r) {
 		if(l == r)
 			return 0;
@@ -19,7 +86,16 @@ public class Solution {
         return inversions;
         
     }
-    
+	
+	//  {2, 1, 3, 1, 2};
+	// 		merge(0, 1)
+	//		m = 0
+	//		aux [2] = [1]
+	//	i = 0
+	//	j = 2
+	//  current = 1
+	// 	inversions = 
+	// 2 <= 1
     static long merge(int[] array, int l, int r){
         int m = (l + r) / 2;
         
@@ -36,7 +112,8 @@ public class Solution {
             } else {
                 aux[current++] = array[j++];
                 
-                // I need to use index "i" to know how many values are before m and this are all inversions!
+                // I need to use index "i" to know how 
+                // many values are before m and this are all inversions!
                 inversions += m - i + 1; 
             }
         }
@@ -60,11 +137,17 @@ public class Solution {
 		System.out.println("Given an array");
 		printArray(array);
 		
-		long count = countInversions(array);
-		System.out.println("Count is " + count);
+		//long count = countInversions(array);
+		//System.out.println("Count is " + count);
+		
+		inversions = 0;
+		countInversionsQuick(array);
+		System.out.println("Count is " + inversions);
 		
 		System.out.println("Sorted array: ");
 		printArray(array);
+		
+		
 	}
 	
 	/* A utility function to print array of size n */
